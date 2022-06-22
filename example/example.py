5fd0950686781d3f7288ea6741f0d00fdcc4c61f
EleanorJiang <eleanorjiang630@gmail.com>
2022-06-22 02:42:59 +0200

    update examples

--- example/example.py
+++ example/example.py
@@ -1,4 +1,4 @@
-from blonde.BlonDe import BLOND
+from blonde.BlonDe import BLONDE
 from sacrebleu.metrics import BLEU
 
 
@@ -7,8 +7,7 @@ if __name__ == "__main__":
     ref = ["Qiao looked at the photo and recalled twenty years ago.",
            "This bearded man was her newlywed husband,",
            "yet this was the first time they were meeting with each other.",
-           "So Qiao's heart jolted as soon as she saw him, and she quickly stood up.",
-           "Joe’s heart is squeaky as soon as he saw him, and she quickly stands up."
+           "So Qiao's heart jolted as soon as she saw him, and she quickly stood up."
            ]
     sys_1 = ["Qiao looked at the photo and recalled twenty years ago.",
            "This bearded man is her newlywed husband.",
@@ -18,7 +17,7 @@ if __name__ == "__main__":
            "The man with the beard was her newly-wed husband.",
            "However, that was the first time they met.",
            "So as soon as Qiao saw him, her heart became squeaky, and she swiftly stood up."]
-    blond = BLOND()
+    blond = BLONDE()
     bleu = BLEU()
     for sys in [sys_1, sys_2]:
         score = blond.corpus_score([sys], [[ref]])
